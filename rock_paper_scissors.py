1bf4f9242239d306af1952950fc4bba50f36299f
nahyan007 <shorifrex@gmail.com>
2024-07-02 00:32:13 +0600

    logic creating of win condition

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -8,7 +8,30 @@ player = None
 while player not in choices:
     player = input("rock, paper or scissor: ").lower()
     print("Please choose from rock, paper or scissor ")
-
-print("computer: ", computer)
-print("player: ", player)
-print("result")
+    if player == "rock":
+        if computer == "paper":
+            print("computer: ", computer)
+            print("player: ", player)
+            print("You Lose")
+        if computer == "scissor":
+            print("computer: ", computer)
+            print("player: ", player)
+            print("You Win")
+    elif player == "paper":
+        if computer == "scissor":
+            print("computer: ", computer)
+            print("player: ", player)
+            print("You Lose")
+        if computer == "rock":
+            print("computer: ", computer)
+            print("player: ", player)
+            print("You Win")
+    elif player == "scissor":
+        if computer == "rock":
+            print("computer: ", computer)
+            print("player: ", player)
+            print("You Lose")
+        if computer == "paper":
+            print("computer: ", computer)
+            print("player: ", player)
+            print("You Win")
