ee2320f9c120228b13c8960fb924b692dbfb9285
nahyan007 <shorifrex@gmail.com>
2024-07-02 00:21:15 +0600

    player choice from the choices

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,5 +1,14 @@
 import random #random module used for to choose computer random things 
 
-Choices = ["rock", "paper", "scissors"]
+choices = ["rock", "paper", "scissor"]
 
-print(random.choice(Choices))
+computer = random.choice(choices)
+player = None
+
+while player not in choices:
+    player = input("rock, paper or scissor: ").lower()
+    print("Please choose from rock, paper or scissor ")
+
+print("computer: ", computer)
+print("player: ", player)
+print("result")
